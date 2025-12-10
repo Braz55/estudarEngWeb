0c90b63dbf4a1372f3b5da181ce4dff14d1eebe0
Braz55 <pm7703125@gmail.com>
2025-12-10 17:39:25 +0000

    ficha 2 resolvida

---
+++ ficha2_2/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ficha2_2.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="The field {0} is mandatory")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="the field is mandatory")]
+        [Range(18,100,ErrorMessage ="must be between {18} {100}")]
+        public int Age { get; set; }
+    }
+}
